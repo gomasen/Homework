9ede24fbff8aa2f9a81c4eb377df7373fef36142
zawaume <umena.coala@gmail.com>
2019-12-16 13:47:32 +0900

    Conpleated 3D Tree Program

--- 05tree.py
+++ 05tree.py
@@ -4,8 +4,11 @@ from OpenGL.GLUT import *
 import sys
 
 THETA = 25
+NUMBER_OF_BLANCH = 3
 R = 0.8
+
 n = 0
+r = 0.0
 
 def drawLine(l):
     glBegin(GL_LINE_STRIP)
@@ -19,6 +22,7 @@ def v(k, l):
         glPushMatrix()
 
         glRotated(rotation, 0.0, 0.0, 1.0)
+
         drawLine(l)
         glTranslated(0.0,l,0.0)
         
@@ -31,34 +35,48 @@ def v(k, l):
         if n < k:
             return 0
         else:
-            v1(THETA)
-            glPopMatrix()
+            for i in range(3):
+                glRotated(360 / NUMBER_OF_BLANCH, 0.0, 1.0, 0.0)
+                v1(THETA)
+                glPopMatrix()
 
-            v1(-THETA)
-            glPopMatrix()
-    
+def idle():
+    glutPostRedisplay()
 
 def resize(w, h):
-    r = 5.0
     glViewport(0, 0, w, h)
+    
+    glMatrixMode(GL_PROJECTION)
     glLoadIdentity()
-    glOrtho(-r, r, -r, r, -r, r)
+    gluPerspective(30.0, w/h, 1.0, 100.0)
+
+    glMatrixMode(GL_MODELVIEW)
 
 def init():
     glClearColor(0.0, 0.0, 0.0, 1.0)
 
 def display():
+    global r
+
+    glLoadIdentity()
+    gluLookAt(10.0, 10.0, 10.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0)
+    glRotated(r, 0.0, 1.0, 0.0)
+
     glClear(GL_COLOR_BUFFER_BIT)
     v(0,1)
     glFlush()
 
 def mouse(button, state, x, y):
-    global n
+    global n, r
 
     if button == GLUT_LEFT_BUTTON and state == GLUT_DOWN:
         n += 1
     elif button == GLUT_RIGHT_BUTTON and state == GLUT_DOWN:
         n = 0
+    elif button == 3:
+        r = 0 if r == 359 else r+1
+    elif button == 4:
+        r = 359 if r == 0 else r-1
 
     display()
 
